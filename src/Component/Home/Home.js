a2ccb59b95e54a4bd3554a3c694c60d818c143fe
unknown <sharifmahmud825@gmail.com>
2021-11-29 00:33:03 +0600

    basic components created

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Contact from '../Contact/Contact';
+import MyProjects from '../MyProjects/MyProjects';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>This is home page</h2>
+      {/* <MyProjects></MyProjects> */}
+      {/* <Contact></Contact> */}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Component/MyProjects/MyProjects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProjects = () => {
+  return (
+    <div>
+      <h2>THis is my project</h2>
+    </div>
+  );
+};
+
+export default MyProjects;
