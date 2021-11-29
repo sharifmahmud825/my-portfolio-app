534e2db5429409c05474f7389cb0941003cb7e94
unknown <sharifmahmud825@gmail.com>
2021-11-29 18:18:28 +0600

    contact added

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -3,7 +3,23 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <h2>this is contact</h2>
+      <div className='container'>
+        <h2 className='text-center mt-5'>Contact Me</h2>
+        <hr />
+        <div className='row'>
+          <div className='col-md-3'></div>
+          <div className='col-md-6'>
+            <input
+              type='email'
+              name=''
+              id=''
+              placeholder='Email'
+              className='form-control bg-dark'
+            />
+          </div>
+          <div className='col-md-3'></div>
+        </div>
+      </div>
     </div>
   );
 };
